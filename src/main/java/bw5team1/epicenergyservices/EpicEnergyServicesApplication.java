2132e0c8f4f884310d9ce07343cf6ef221d84482
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-22 10:08:37 +0200

    Initial commit

---
+++ src/main/java/bw5team1/epicenergyservices/EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package bw5team1.epicenergyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bw5team1/epicenergyservices/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package bw5team1.epicenergyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
