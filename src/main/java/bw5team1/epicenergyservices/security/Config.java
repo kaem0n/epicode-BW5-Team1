0b00f24e8dede8e969a2ef916e6a9bc6654e7880
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-26 11:50:22 +0200

    cors in config

--- src/main/java/bw5team1/epicenergyservices/security/Config.java
+++ src/main/java/bw5team1/epicenergyservices/security/Config.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -32,4 +37,19 @@ public class Config {
     PasswordEncoder getBCrypt(){
         return new BCryptPasswordEncoder(11);
     }
-}
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5177"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+
+
+    }}
